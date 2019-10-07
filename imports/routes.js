1221a7d407b551bc06182f9df503d1b4cb8b6264
alexanderkif <alexander_kif@mail.ru>
2019-10-07 18:27:47 +0400

    checking taskId added

--- imports/routes.js
+++ imports/routes.js
@@ -1,3 +1,4 @@
+import { check } from 'meteor/check';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { Tasks } from './api/tasks';
@@ -19,6 +20,7 @@ FlowRouter.route('/create', {
 FlowRouter.route('/edit/:taskId', {
   name: 'Edit.form',
   action(params) {
+    check(params.taskId, String);
     BlazeLayout.render('edit_form', { task: Tasks.findOne(params.taskId) });
   },
 });
